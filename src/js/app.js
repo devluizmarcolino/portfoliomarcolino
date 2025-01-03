0332cca6a433feca36838353e94ff0dec0ad8922
devluizmarcolino <devluizmarcolino@gmail.com>
2025-01-03 15:34:00 -0300

    feat: nova seção sobre mim adicionada (html/css/js)

--- src/js/app.js
+++ src/js/app.js
@@ -17,3 +17,32 @@ navItems.forEach((item) => {
     closeIcon.classList.add("hidden");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  
+  const navLinks = document.querySelectorAll("nav ul li a");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+
+  const skillItems = document.querySelectorAll(".skill-item");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+  skillItems.forEach((item) => {
+    observer.observe(item);
+  });
+});
